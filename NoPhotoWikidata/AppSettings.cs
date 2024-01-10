bcc832cc766494690b5fe34728f59898f25afd9e
vankos <14087195+vankos@users.noreply.github.com>
2024-01-10 22:52:10 +0700

    move GetGpx to view model as command

--- NoPhotoWikidata/AppSettings.cs
+++ NoPhotoWikidata/AppSettings.cs
@@ -1,4 +1,8 @@
 ﻿using System.ComponentModel;
+using System.Globalization;
+using System.Windows.Input;
+using WikidataCommon;
+using Binding = WikidataCommon.Binding;
 
 namespace NoPhotoWikidata
 {
@@ -17,6 +21,8 @@ namespace NoPhotoWikidata
             "appartments in",
         ];
         private const string exludedDescriptionWordsSettingsKey = "ExludedDescriptionWordsSettingsKey";
+
+        private const string DefualtGpxFileNamePrefix = "NoPhotoLocations_.";
 
         public AppSettings()
         {
@@ -47,6 +53,54 @@ namespace NoPhotoWikidata
             }
         }
 
+        public ICommand GetGpxCommand => new Command(async () => await GetGpx());
+
         public event PropertyChangedEventHandler? PropertyChanged;
+
+        private async Task GetGpx()
+        {
+            Microsoft.Maui.Devices.Sensors.Location? location = await Geolocation.Default.GetLastKnownLocationAsync();
+            if (location == null)
+            {
+                //await DisplayAlert("No luck", "Unable to get last location", "OK");
+                return;
+            }
+
+            Coordinates coordinates = new()
+            {
+                Lattittude = location.Latitude,
+                Longitude = location.Longitude,
+            };
+
+            double searchRadiusDegrees = double.Parse(SearchRadiusDegrees, CultureInfo.InvariantCulture);
+            WikidataQueryResult? queryResult = QueryService.GetWikiLocationsForLocation(coordinates, searchRadiusDegrees);
+            if (queryResult == null)
+            {
+                return;
+            }
+
+            List<Binding> locations = queryResult.results.bindings;
+            IEnumerable<Binding> locationsWithoutImage = LocationFilter.FilterByDoesntHaveImage(locations);
+            string[] exclusions = DescriptionExclusions.Split(Environment.NewLine);
+            IEnumerable<Binding> filteredLocations = LocationFilter.FilterByHaveExlusionsInDescription(locationsWithoutImage, exclusions);
+            string gpx = GpxGenerator.GenerateGpxFromWikidataResult(filteredLocations);
+            string fileName = GetFileName(coordinates);
+            string filePath = Path.Combine(FileSystem.CacheDirectory, fileName);
+            File.WriteAllText(filePath, gpx);
+            await Launcher.Default.OpenAsync(new OpenFileRequest(fileName, new ReadOnlyFile(filePath)));
+        }
+
+        private static string GetFileName(Coordinates coordinates)
+        {
+            string fileNamePrefix = DefualtGpxFileNamePrefix;
+            string? locationName = QueryService.GetLocationNameFromCoordinates(coordinates);
+            if (locationName != null)
+            {
+                fileNamePrefix = locationName + "_";
+            }
+
+            string fileName = fileNamePrefix + DateTime.Now + ".gpx";
+            return fileName;
+        }
     }
 }
--- NoPhotoWikidata/MainPage.xaml.cs
+++ NoPhotoWikidata/MainPage.xaml.cs
@@ -1,66 +1,14 @@
-﻿using System.Globalization;
-using WikidataCommon;
-using Binding = WikidataCommon.Binding;
-
-namespace NoPhotoWikidata
+﻿namespace NoPhotoWikidata
 {
     public partial class MainPage : ContentPage
     {
         private readonly AppSettings context;
-
-        private const string DefualtGpxFileNamePrefix = "NoPhotoLocations_.";
 
         public MainPage()
         {
             InitializeComponent();
             context = new AppSettings();
             BindingContext = context;
-        }
-
-        private async void GetGpxButton_Clicked(object sender, EventArgs e)
-        {
-            Microsoft.Maui.Devices.Sensors.Location? location = await Geolocation.Default.GetLastKnownLocationAsync();
-            if(location == null)
-            {
-                await DisplayAlert("No luck", "Unable to get last location", "OK");
-                return;
-            }
-
-            Coordinates coordinates = new()
-            {
-                Lattittude = location.Latitude,
-                Longitude = location.Longitude,
-            };
-
-            double searchRadiusDegrees = double.Parse(context.SearchRadiusDegrees, CultureInfo.InvariantCulture);
-            WikidataQueryResult? queryResult = QueryService.GetWikiLocationsForLocation(coordinates, searchRadiusDegrees);
-            if (queryResult == null)
-            {
-                return;
-            }
-
-            List<Binding> locations = queryResult.results.bindings;
-            IEnumerable<Binding> locationsWithoutImage = LocationFilter.FilterByDoesntHaveImage(locations);
-            string[] exclusions = context.DescriptionExclusions.Split(Environment.NewLine);
-            IEnumerable<Binding> filteredLocations = LocationFilter.FilterByHaveExlusionsInDescription(locationsWithoutImage, exclusions);
-            string gpx = GpxGenerator.GenerateGpxFromWikidataResult(filteredLocations);
-            string fileName = GetFileName(coordinates);
-            string filePath = Path.Combine(FileSystem.CacheDirectory, fileName);
-            File.WriteAllText(filePath, gpx);
-            await Launcher.Default.OpenAsync(new OpenFileRequest(fileName, new ReadOnlyFile(filePath)));
-        }
-
-        private static string GetFileName(Coordinates coordinates)
-        {
-            string fileNamePrefix = DefualtGpxFileNamePrefix;
-            string? locationName = QueryService.GetLocationNameFromCoordinates(coordinates);
-            if (locationName != null)
-            {
-                fileNamePrefix = locationName + "_";
-            }
-
-            string fileName = fileNamePrefix + DateTime.Now + ".gpx";
-            return fileName;
         }
     }
 }
